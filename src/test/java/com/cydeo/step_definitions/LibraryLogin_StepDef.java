e91c431616028fc5f2857b15b1172ea4757df732
adamar <aysun114@hotmail.com>
2023-07-12 10:16:19 -0500

    LibraryLogin_StepDef added

---
+++ src/test/java/com/cydeo/step_definitions/LibraryLogin_StepDef.java
@@ -0,0 +1,24 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LibraryLogin_StepDef {
+
+
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+
+    }
+
+    @When("I login as a librarian")
+    public void i_login_as_a_librarian() {
+
+    }
+
+    @Then("dashboard should be displayed")
+    public void dashboard_should_be_displayed() {
+
+    }
+}
