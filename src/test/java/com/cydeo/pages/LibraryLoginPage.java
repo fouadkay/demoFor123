f38081a7ef87d4a03036ad1aa2ecdb68f586d776
adamar <aysun114@hotmail.com>
2023-07-12 10:55:35 -0500

    LibraryLogin_StepDef added

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,5 +1,6 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,5 +20,18 @@ public class LibraryLoginPage {
 
     @FindBy(xpath = "//button[.='Sign in']")
     public WebElement signinBtn;
+
+
+    public void loginAsLibrarian(){
+       emailInput.sendKeys(ConfigurationReader.getProperty("lib22_user"));
+       passwordInput.sendKeys(ConfigurationReader.getProperty("lib22_pass"));
+       signinBtn.click();
+    }
+
+    public void loginAsLibrarian(String email, String password){
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        signinBtn.click();
+    }
 
 }
--- src/test/java/com/cydeo/step_definitions/LibraryLogin_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/LibraryLogin_StepDef.java
@@ -1,24 +1,41 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.LibraryLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LibraryLogin_StepDef {
 
+    LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("qa2_url"));
     }
 
     @When("I login as a librarian")
     public void i_login_as_a_librarian() {
+//      libraryLoginPage.emailInput.sendKeys(ConfigurationReader.getProperty("lib22_user"));
+//      libraryLoginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("lib22_pass"));
+//      libraryLoginPage.signinBtn.click();
+
+      //  libraryLoginPage.loginAsLibrarian();
+
+        libraryLoginPage.loginAsLibrarian(ConfigurationReader.getProperty("lib22_user"),ConfigurationReader.getProperty("lib22_pass"));
 
     }
 
     @Then("dashboard should be displayed")
     public void dashboard_should_be_displayed() {
+
+        String actualURL = Driver.getDriver().getCurrentUrl();
+        String expectedURL = "dashboard";
+
+        Assert.assertTrue("dashboard is not appear on URL",actualURL.contains(expectedURL));
 
     }
 
